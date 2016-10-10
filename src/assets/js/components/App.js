985b2ce712ee097164ec0fa244670d1029e6d4a0
Matine Chabrier <matine.chabrier@iris-worldwide.com>
2016-10-10 11:25:28 +0100

    Added more to docs but moved to readme file

--- src/assets/js/components/App.js
+++ src/assets/js/components/App.js
@@ -12,22 +12,41 @@ var App = React.createClass({
 	render : function() {
 
 		var config = {
+			selectedStartDefaultString : "Start date",
+			selectedEndDefaultString : "End date",
 			disabledDays : [
+				{
+					firstDay : new Date("October 16, 2016"),
+					lastDay : new Date("October 19, 2016")
+				},
+				{
+					firstDay : new Date("December 4, 2016"),
+					lastDay : new Date("December 4, 2016"),
+				}
 			],
 			classNames : {
+				datepicker: "my-datepicker my-datepicker-one",
+				dateInputsWrapper: "date-inputs-wrapper",
+				dateInput: "date-input",
+				dateInputStart: "date-input-start",
+				dateInputEnd: "date-input-end",
+				calendarWrapper: "my-calendar-wrapper",
+				calendar: "my-calendar"
 			},
 			theme : {
 				inputs : {
+					activeBackgroundColor: "#ffcccc",
+					activeColor: "#565a5c"
 				},
 				days : {
+					selectedBackgroundColor:"#ff4d4d",
+					inbetweenBackgroundColor: "#ffcccc",
 				}
 			}
 		}
 
 		return (
-			<div>
-				<DatePicker userConfig={config}></DatePicker>
-			</div>
+			<DatePicker userConfig={config}></DatePicker>
 		)
 	}
 });
